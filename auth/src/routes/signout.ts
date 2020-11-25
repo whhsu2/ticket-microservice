e36538d78e896e2055c65a4a8a69fc45c34f9f56
morris754 <morris754@gmail.com>
2020-11-25 00:27:04 -0500

    add error handling

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
-    res.send('Hi there!')
-})
+router.post('/api/users/signout', (req, res) => {
+  res.send('Hi there!');
+});
 
-export { router as signoutRouter }
+export { router as signoutRouter };
