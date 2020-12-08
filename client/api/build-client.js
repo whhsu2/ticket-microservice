f7f91d7967c19e2bcd49b000ca84312ff0c81a6d
morris754 <morris754@gmail.com>
2020-12-07 21:35:26 -0500

    add client service

---
+++ client/api/build-client.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const axiosApi = ({ req }) => {
+    if ( typeof window === 'undefined') {
+        // We are on the server
+        return axios.create({
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
+            headers: req.headers
+        })
+    } else {
+        // we are on the browser
+        return axios.create({
+            baseURL: '/',
+        })
+    }
+}
+
+export default axiosApi
